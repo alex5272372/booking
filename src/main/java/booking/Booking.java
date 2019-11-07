0269e7b8c1ccd4725952747ccb7c5c795a67af53
vetalkonev <vetalkonev13@gmail.com>
2019-11-07 23:13:46 +0200

    booking dao

--- src/main/java/booking/Booking.java
+++ src/main/java/booking/Booking.java
@@ -2,21 +2,26 @@ package booking;
 
 import users.User;
 
+import java.util.Random;
+
 public class Booking {
-    private static int maxId;
-    private int id;
     private int tripId;
+    private int bookingId;
+    private int count;
     private User user;
 
     static {
         maxId = 0;
     }
 
-    public Booking(int tripId, User user) {
-        this.id = maxId++;
+    public Booking(int tripId, int bookingId, int count, User user) {
         this.tripId = tripId;
+        this.bookingId = bookingId;
         this.user = user;
+        this.count = count;
     }
+
+
 
     public int getTripId() {
         return tripId;
@@ -44,5 +49,22 @@ public class Booking {
 
     public String toString() {
         return String.format("|%10d |%10d | %-40s|", id, tripId, user.getFullName());
+    }
+    public int generateId() {
+        int id = getRandomInt(1000, 9999);
+        while (isNotUniqueId(id)) {
+            id = getRandomInt(1000, 9999);
+        }
+        return id;
+    }
+
+    int getRandomInt(int from, int to) {
+        Random random = new Random();
+        return from + random.nextInt(to - from + 1);
+    }
+
+    boolean isNotUniqueId(int id) {
+        if (tripController == null) return false;
+        return tripController.getAllTrips().stream().anyMatch(trip -> trip.getId() == id);
     }
 }
--- src/main/java/booking/BookingDaoImpl.java
+++ src/main/java/booking/BookingDaoImpl.java
@@ -1,33 +1,78 @@
 package booking;
 
 import users.User;
+
+import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookingDaoImpl implements BookingDao{
-    private List<Booking> bookingList;
 
-    public BookingDaoImpl() {
-        this.bookingList = new ArrayList<Booking>();
-    }
+    private List<Booking> bookingList = new ArrayList<>();
+
+//    public BookingDaoImpl() {
+//        this.bookingList = new ArrayList<Booking>();
+//    }
 
     public List<Booking> getAllBookings() {
-        return bookingList;
+        return new ArrayList<>(bookingList) ;
     }
 
     public Booking getBooking(int id) {
-        return null;
+        return getAllBookings().stream().filter(booking -> booking.getId() == id).findFirst().get();
     }
 
-    public void addBooking(int tripId, User user) {
-        bookingList.add(new Booking(tripId, user));
+    public void addBooking(int tripId,int count, User user) {
+        bookingList.add(new Booking(tripId, count, user));
     }
 
-    public void updateBooking(int id, int tripId, User user) {
-
+    public void updateBooking(int tripId,int bookingId,int count,User user) {
+        if (isPresentId(id)) {
+            getBooking(bookingId).setTripId(date);
+            getBooking(bookingId).setBookingId(bookingId);
+            getBooking(bookingId).setCount(count);
+            getBooking(bookingId).setUser(user);
+        }
     }
 
     public void deleteBooking(int id) {
+        if (isPresentId(id)) tripList.remove(getTrip(id));
 
+    }
+
+// read file, write file, transform toString
+
+    public void readBooking() throws IOException {
+        File tripFile = new File("./booking.csv");
+        BufferedReader reader = new BufferedReader(new FileReader(tripFile));
+        List<String> strList = reader.lines().collect(Collectors.toList());
+        strList.forEach(str -> {
+            try {
+                Booking booking = transformStringToBooking(str);
+                bookingList.add(booking);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public void writeBooking() throws IOException {
+        File bookingFile = new File("./booking.csv");
+        FileWriter fwClean = new FileWriter(bookingFile);
+        PrintWriter pwClean = new PrintWriter(fwClean);
+        pwClean.close();
+        FileWriter fw = new FileWriter(bookingFile);
+        PrintWriter pw = new PrintWriter(fw, true);
+        getAllBookings().forEach(booking -> pw.println(booking.toCsvString()));
+
+    }
+    public Booking transformStringToBooking(String str) throws ParseException {
+        String[] arr = str.split(";");
+        int tripId = Integer.parseInt(arr[0]);
+        int bookingID = Integer.parseInt(arr[1]);
+        int count = Integer.parseInt(arr[2]);
+        User user = User.getUserByLogin(arr[3]);
+        return new Booking(tripId, bookingID, count, user);
     }
 }
